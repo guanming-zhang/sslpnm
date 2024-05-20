4b59fb18dc628c1477603574c1356bde1186b3ae
Guanming Zhang <guanming2718@gmail.com>
2024-05-20 18:22:02 -0400

    test

--- model/loss_module.py
+++ model/loss_module.py
@@ -5,9 +5,20 @@ class CrossEntropy:
         self.n_veiws = n_views
     def __call__(self,preds,labels):
         if isinstance(preds,list): # for augumented data
-            loss = 0.0
-            for i in range(len(preds)):
-                loss += self.loss(preds[i],labels[i])
+            #imgs = torch.cat(imgs,dim=0)
+            #labels = torch.cat(labels,dim=0)
+            #imgs,labels = imgs.to(self.device),labels.to(self.device)
+            #preds = self.net(imgs)
+            #loss = self.loss(preds,labels)
+
+            preds = torch.cat(preds,dim=0)
+            labels = torch.cat(labels,dim=0)
+            loss = self.loss(preds,labels)
+
+
+            #loss = 0.0
+            #for i in range(len(preds)):
+            #    loss += self.loss(preds[i],labels[i])
         else:
             loss = self.loss(preds,labels)
         return loss
